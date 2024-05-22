52a6dcb78eed04118bbc8473937552bfab95fe44
jrmblt <thanawat.j@blttech.net>
2024-05-23 01:44:32 +0700

    update deploy ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'npm start',
+    watch: '.'
+  }],
+  deploy : {
+    production : {
+      user : 'ratakorn',
+      host : '10.252.93.208',
+      ref  : 'origin/deploy',
+      repo : 'git@github.com:deeptech-kmitl/e-logbook-web-application.git',
+      path : '/home/ratakorn',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install --force && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes',
+    }
+  }
+};
